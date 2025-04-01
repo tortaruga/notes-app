60f44451e779d29e34045ddcdfd1c3283a36c384
tortaruga <jojoway77@gmail.com>
2025-04-01 15:35:27 +0200

    added README.md

---
+++ README.md
@@ -0,0 +1,7 @@
+### overview
+
+web app to take, edit, and delete notes. made with react, uses indexedDB to store the notes locally.
+
+### features to add
+- [ ] delete all notes
+- [ ] choose note color
--- src/components/Blabberings.jsx
+++ src/components/Blabberings.jsx
@@ -10,7 +10,7 @@ export default function Blabberings() {
           to store your data, which means you can close or refresh the page, and your notes will remain saved. 
           However, <strong>if you clear site data or uninstall the browser, your notes will be lost.</strong></p>
           
-          <p>interested in a todo list version of this website? <a href="#">check here!</a></p>
+          <p>interested in a todo list version of this website? <a href="todo-todo-web-app.netlify.app/" target="_blank">check here!</a></p>
 
           <p className="credits">icons by <a href="https://www.svgrepo.com/" target="_blank">SVGrepo.</a> coded by <a href="https://github.com/tortaruga" target="_blank">tortaruga</a></p>
         </div>
